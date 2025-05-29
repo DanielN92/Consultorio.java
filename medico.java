ace55a80d9e2d52fd34142a86bc98a84363877cb
DanielN92 <al03089603@tecmilenio.mx>
2025-05-28 19:38:51 -0600

    se agregan modulo main y sus carecteristicas

---
+++ medico.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.consultorio;
+
+/**
+ *
+ * @author Daniel Noriega R
+ */
+public class medico {
+    static int contador = 1;
+    int id;
+    String nombre;
+    String especialidad;
+
+    public medico() {
+        this.id = contador++;
+    }
+}
